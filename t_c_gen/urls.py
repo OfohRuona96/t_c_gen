6925cead74f476a78294470d0c4c437487094483
saheedibrahim <saolabram@gmail.com>
2022-12-03 09:20:47 +0100

    test

--- t_c_gen/urls.py
+++ t_c_gen/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('teamhyena2', include('teamhyena2.urls')),
+    path('teamhyena2/', include('teamhyena2.urls')),
 ]
